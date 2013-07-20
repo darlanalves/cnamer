eb53586d6c71f931f3c9224a624865dbead4c8c8
Samuel Ryan <sam@samryan.co.uk>
2013-07-20 03:35:23 +0100

    fixed a brazillian bugs

--- cnamer.php
+++ cnamer.php
@@ -1,19 +1,21 @@
 <?php
 
-    $domain = "cnamer.com";
+    $domain = "cnamer.dev";
     $cname = $_SERVER['HTTP_HOST'];
+    $destination_domain = str_replace('.' . $domain, "", $cname);
     $querystring = substr($_SERVER['REQUEST_URI'], 1);
     
-    $options_str = explode("-opts-", $cname);
+    $options_str = explode("-opts-", $destination_domain);
+    $options_string = $options_str[1];
     
-    $destination_domain = $options_str[0];
-    $options_string = str_replace($domain, "", $options_str[1]);
+    if(isset($options_str[1])) {
+        $destination_domain = $options_str[0];
+        preg_match_all('%(([\w]+).([\w]+))%', $options_string, $options_matches);
+        foreach ($options_matches[2] as $key => $option) {
+            $option_list[$option][] = $options_matches[3][$key];
+        }
+    }
     
-    preg_match_all('%(([\w]+).([\w]+))%', $options_string, $options_matches);
-    
-    foreach ($options_matches[2] as $key => $option)
-        $option_list[$option][] = $options_matches[3][$key];
-
     $default_options = array(
         "protocol" => array(
             "default" => "http",
@@ -29,24 +31,21 @@
             "join" => "/",
         ),
     );
-    
+
     foreach($default_options as $dkey => $dproperties) {
         if(count($option_list[$dkey]) > 1) {
             $value = implode($dproperties["join"], $option_list[$dkey]);
         } else {
-            $value = $option_list[$dkey][0];
+            $value = $option_list[$dkey][0] ?: $dproperties['default'];
         }
-        
-        if(!isset($option_list[$dkey])) 
-            $value = $default_options[$dkey]["default"];
-        
+
         if($value === "true" || $value === "false")
             $value = ($value === "true") ? true : (($value === "false") ? false : $value);
-        
+
         $options[$dkey] = ($value ?: false);
     }
-    
-    $url = ($options['protocol']) . '://' . $destination_domain . '/' . ($options['querystring'] ?: false) . '/' . ($options['query'] ? $querystring : false);
+        
+    $url = ($options['protocol']) . '://' . $destination_domain . '/' . ($options['querystring'] ?: false) . ($options['query'] ? $querystring : false);
     
     Header('HTTP/1.0 301');
     Header('Location: ' . $url);
