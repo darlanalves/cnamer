930ee744f8d0eec96a8ccca350321fab752d42b7
Samuel Ryan <sam@samryan.co.uk>
2013-07-22 03:46:08 +0100

    bootstrap + cnamer class base

---
+++ bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+define('cnamer_domain', 'cnamer.com');
+
+spl_autoload_register(function($class_name) {
+    $file = __DIR__ . '/lib' . DIRECTORY_SEPARATOR . strtr($class_name, '\\', DIRECTORY_SEPARATOR) . ".php";
+    if(is_readable($file))
+        require_once $file;
+});
---
+++ lib/Cnamer/Cnamer.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Cnamer;
+
+class Cnamer{
+    
+}
