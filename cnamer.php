be2b206437f1dae47146bf57f41f0d874abff1ee
Samuel Ryan <sam@samryan.co.uk>
2013-07-24 10:14:18 +0100

    cnamer class v1

--- cnamer.php
+++ cnamer.php
@@ -1,3 +1,17 @@
 <?php
 
 require_once __DIR__ . '/bootstrap.php';
+
+$request = array(
+    "domain" => $_SERVER['HTTP_HOST'],
+    "uri" => substr($_SERVER['REQUEST_URI'], 1) != "" ? $_SERVER['REQUEST_URI'] : false,
+);
+
+$cnamer = new Cnamer\Cnamer("appledave.co.uk");
+
+    // CATCH EXCEPTIONS AND RENDER HELPFUL ERROR PAGE
+
+// LOG THE STUFF
+// maybe possibly have like, a live page that displays redirects done using pubnub?
+
+// perform redirect
--- lib/Cnamer/Cnamer.php
+++ lib/Cnamer/Cnamer.php
@@ -4,4 +4,67 @@ namespace Cnamer;
 
 class Cnamer{
     
+    function __construct($domain) {
+        $cname_record = $this->lookup('CNAME', $domain);
+        $domain_type = 'sub';
+        if(!$cname_record || $cname_record['type'] != "CNAME") {
+            $domain_type = 'root';
+            if(!$cname_record = $this->lookup('CNAME', "cnamer.{$domain}"))
+                throw new Exception('Record cannot be found');
+        }
+        
+        if($cname_record['target'] == 'txt.' . cnamer_domain) {
+            $txt_id = ($domain_type == 'root') ? ('cnamer-root.' . $domain) : 'cnamer-' . $domain;
+            if(!$txt_records = $this->lookup('TXT', $txt_id))
+                throw new Exception('TXT Configuration not found');
+            
+            $domain_config = json_decode($txt_records[0]['txt'], true);
+        } else {
+            // cname configuration
+            $domain_config = array();
+        }
+        
+        $configuration = $this->compile_config($domain_config);
+        
+        $redirect = array(
+            "destination" => "",
+        );
+        
+        return $redirect;
+    }
+    
+    function lookup($type, $domain, $cache = 500) {
+        // look in cache for data
+        
+        // retrieve data
+        $lookup = "lookup_{$type}";
+        return $this->$lookup($domain);
+        
+        // cache data
+        
+        // return data
+    }
+    
+    function compile_config($config) {
+        // return the compiled configuration
+        return array();
+    }
+    
+    function lookup_CNAME($domain) {
+        $record = dns_get_record($domain, DNS_CNAME);
+        return $record ? $record[0] : false;
+    }
+    
+    function lookup_TXT($domain) {
+        return dns_get_record($domain, DNS_TXT);
+    }
+    
+    function cache_retrieve($domain) {
+        
+    }
+    
+    function cache_store($data) {
+        
+    }
+    
 }
