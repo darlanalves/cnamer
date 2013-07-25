c98cb2bd8f2ebacc22958ec4c7f8f6e279b3448c
Samuel Ryan <sam@samryan.co.uk>
2013-07-25 06:04:46 +0100

    calculate and perform redirect n stuff

--- cnamer.php
+++ cnamer.php
@@ -7,11 +7,23 @@ $request = array(
     "uri" => substr($_SERVER['REQUEST_URI'], 1) != "" ? $_SERVER['REQUEST_URI'] : false,
 );
 
-$cnamer = new Cnamer\Cnamer("appledave.co.uk");
+$request = array(
+    "domain" => "uristring.appledave.co.uk",
+    "uri" => substr($_SERVER['REQUEST_URI'], 1) != "" ? $_SERVER['REQUEST_URI'] : false,
+);
 
-    // CATCH EXCEPTIONS AND RENDER HELPFUL ERROR PAGE
+try {
+    #$cnamer = new Cnamer\Cnamer("opts.appledave.co.uk");
+   $cnamer = new Cnamer\Cnamer($request);
+   $redirect = $cnamer->redirect();
+   #$cnamer = new Cnamer\Cnamer("appledave.co.uk");
+} catch (Exception $e) {
+    print_r($e);
+    // render helpful error page
+}
 
 // LOG THE STUFF
 // maybe possibly have like, a live page that displays redirects done using pubnub?
 
-// perform redirect
+# Header("HTTP/1.0 {$redirect['statuscode']}");
+# Header("Location: {$redirect['destination']}");
--- lib/Cnamer/Cnamer.php
+++ lib/Cnamer/Cnamer.php
@@ -1,36 +1,72 @@
 <?php
 
 namespace Cnamer;
+use Exception;
 
 class Cnamer{
     
-    function __construct($domain) {
+    function __construct($request) {
+        $domain = $request['domain'];
+        
         $cname_record = $this->lookup('CNAME', $domain);
         $domain_type = 'sub';
         if(!$cname_record || $cname_record['type'] != "CNAME") {
             $domain_type = 'root';
             if(!$cname_record = $this->lookup('CNAME', "cnamer.{$domain}"))
-                throw new Exception('Record cannot be found');
+                throw new \Exception('Record cannot be found');
         }
         
         if($cname_record['target'] == 'txt.' . cnamer_domain) {
             $txt_id = ($domain_type == 'root') ? ('cnamer-root.' . $domain) : 'cnamer-' . $domain;
             if(!$txt_records = $this->lookup('TXT', $txt_id))
-                throw new Exception('TXT Configuration not found');
+                throw new \Exception('TXT Configuration not found');
             
             $domain_config = json_decode($txt_records[0]['txt'], true);
+            
+            if(substr($domain_config['destination'], -1) == '/')
+                $domain_config['destination'] = substr($domain_config['destination'], 0, -1);
+            
+            $domain_config['source'] = 'txt';
         } else {
-            // cname configuration
-            $domain_config = array();
+            $target = $cname_record['target'];
+            $opts = explode("-opts-", $target);
+            
+            $domain_config['source'] = 'cname';
+            $domain_config["destination"] = $opts[0];
+            
+            if(isset($opts[1])) {
+                $options = explode("-", str_replace('.' . cnamer_domain, "", $opts[1]));
+                foreach($options as $option) {
+                    $o = explode(".", $option);
+                    $option_values[$o[0]][] = $o[1];
+                }
+                
+                foreach($option_values as $option => $values) {
+                    if(count($values) > 1) {
+                        $domain_config['options'][$option] = $values;
+                    } else {
+                        $domain_config['options'][$option] = $values[0];
+                    }
+                }
+            }
         }
         
+        if(!empty($request['uri']))
+            $domain_config['request_uri'] = $request['uri'];
+        
         $configuration = $this->compile_config($domain_config);
+        $destination = $this->render_destination($configuration);
         
         $redirect = array(
-            "destination" => "",
+            "destination" => $destination,
+            "statuscode" => $configuration['statuscode'],
         );
         
-        return $redirect;
+        $this->redirect = $redirect;
+    }
+    
+    function redirect() {
+        return $this->redirect;
     }
     
     function lookup($type, $domain, $cache = 500) {
@@ -47,7 +83,51 @@ class Cnamer{
     
     function compile_config($config) {
         // return the compiled configuration
-        return array();
+        $options = array(
+            "protocol" => array(
+                "value" => "http",
+                "join" => false,
+                "unsupported_type" => "txt",
+            ),
+            "statuscode" => array(
+                "value" => "301",
+                "join" => false,
+            ),
+            "uri" => array(
+                "value" => false,
+                "join" => false,
+            ),
+            "uristring" => array(
+                "value" => false,
+                "join" => "/",
+            ),
+        );
+        
+        $c_options = array();
+        foreach($options as $option => $properties) {
+            if (isset($options[$option]["unsupported_type"]) && $options[$option]["unsupported_type"] == $config['source']) {
+                // do nothing 'cause we're not allowed
+                $c_options[$option] = false;
+            } elseif (isset($config["options"][$option]) && is_array($config["options"][$option]) && $properties['join']) {
+                $c_options[$option] = implode("/", $config['options'][$option]);
+            } elseif (isset($config["options"][$option])) {
+                $c_options[$option] = $config["options"][$option];
+            } else {
+                $c_options[$option] = false;
+            }
+        }
+        
+        return array_merge($c_options, array("destination" => $config['destination'], "request_uri" => $config['request_uri']));
+    }
+    
+    function render_destination($config) {
+        $destination =
+        ($config['protocol'] ? $config['protocol'] . '://' : false) .
+        $config['destination'] .
+        ($config['uristring'] ? '/' . $config['uristring'] : false) .
+        ($config['request_uri'] ? $config['request_uri'] : false);
+        
+        return $destination;
     }
     
     function lookup_CNAME($domain) {
