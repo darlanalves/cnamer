412f9aa31476734f9d8d8bdc33ac3ce804d90032
Samuel Ryan <sam@samryan.co.uk>
2013-07-27 20:42:03 +0100

    cleaned up cnamer.php

--- cnamer/cnamer.php
+++ cnamer/cnamer.php
@@ -7,21 +7,14 @@ $request = array(
     "uri" => substr($_SERVER['REQUEST_URI'], 1) != "" ? $_SERVER['REQUEST_URI'] : false,
 );
 
-$request = array(
-    "domain" => "uritruedev.appledave.co.uk",
-    "uri" => "",
-);
-
 $cnamer = new Cnamer\Cnamer($request);
 
 try {
     $redirect = $cnamer->redirect();
 } catch (Exception $e) {
     error_log(json_encode(array("request" => $request, "error" => $e->getMessage())) . "\n", 3, CNAMER_DIR . 'logs/error.log');
-    die(include("error.php"));
+    Header('Location: http://' . CNAMER_DOMAIN . '/error.php?request=' . $request['domain'] . '&uri=' . $request['uri']);
 }
 
-
-die('during dev we dont want favicon request');
-# Header("HTTP/1.0 {$redirect['statuscode']}");
-# Header("Location: {$redirect['destination']}");
+Header("HTTP/1.0 {$redirect['statuscode']}");
+Header("Location: {$redirect['destination']}");
