5292d4db0eaf13727984543555ca945ef37d95c2
Samuel Ryan <sam@samryan.co.uk>
2013-07-31 20:58:20 +0100

    cnamer.php to work with new Cnamer class

--- cnamer/cnamer.php
+++ cnamer/cnamer.php
@@ -7,14 +7,17 @@ $request = array(
     "uri" => substr($_SERVER['REQUEST_URI'], 1) != "" ? $_SERVER['REQUEST_URI'] : false,
 );
 
-$cnamer = new Cnamer\Cnamer($request);
+$cnamer = new Cnamer\Cnamer();
 
 try {
-    $redirect = $cnamer->redirect();
+    $redirect = $cnamer->redirect($request);
 } catch (Exception $e) {
     error_log(json_encode(array("request" => $request, "error" => $e->getMessage())) . "\n", 3, CNAMER_DIR . 'logs/error.log');
     $destination = 'http://'. CNAMER_DOMAIN . '/error.php?request=' . $request['domain'] . '&uri=' . $request['uri'];
-    http_redirect($destination, false, false, 302);
+    
+    Header("HTTP/1.0 302");
+    Header("Location: {$destination}");
 }
 
-http_redirect($redirect['destination'], false, false, $redirect['options']['statuscode']);
+Header("HTTP/1.0 302");
+Header("Location: {$redirect['destination']}");
