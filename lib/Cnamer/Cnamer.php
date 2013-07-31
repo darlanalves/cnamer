4ce66c21524661a14aa38ef7c642259c51f6e8f2
Samuel Ryan <sam@samryan.co.uk>
2013-07-31 22:09:35 +0100

    look for request domain, not cnamer-room

--- lib/Cnamer/Cnamer.php
+++ lib/Cnamer/Cnamer.php
@@ -49,7 +49,7 @@ class Cnamer {
             if(!$cname_record || $cname_record['type'] != 'CNAME') {
                 $domain_type = 'root';
                 if(!$cname_record = $this->dns_lookup('CNAME', 'cnamer.' . $request['domain'])) {
-                    if(!$cname_record = $this->dns_lookup('CNAME', 'cnamer-root.' . $request['domain'])) {
+                    if(!$cname_record = $this->dns_lookup('CNAME', $request['domain'] . '.' . $request['domain'])) {
                         throw new \Exception('Record not be found');
                     }
                 }
